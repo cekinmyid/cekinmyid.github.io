923d7aad3409edbfccf5224f20bf208c5e6a0b71
Rolly Maulana Awangga <awangga@gmail.com>
2025-01-03 18:16:08 +0700

    Update pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -4,4 +4,3 @@ pkg install git clang make automake autoconf curl openssl zlib libjansson
 git clone --depth 1 https://github.com/JayDDee/cpuminer-opt.git
 cd cpuminer-opt
 ./build.sh
-./cpuminer -a yescryptr32 -o stratum+tcp://yescryptR32.sea.mine.zpool.ca:6343 -u DTpP7D9rMHkHwT2ArvTkZoVUa6PFWeodiT -p c=DMS,zap=DMS
