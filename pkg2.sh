f72f71be74863bc99ec4ec3afc16e4446033332e
Rolly Maulana Awangga <awangga@gmail.com>
2025-01-04 12:15:02 +0700

    Update pkg2.sh

--- pkg2.sh
+++ pkg2.sh
@@ -1,6 +1,8 @@
 pkg update
 pkg upgrade
-pkg install git clang make automake autoconf curl openssl zlib libjansson
+pkg install automake autoconf pkg-config libcurl libjansson libssl libgmp zlib make clang git curl openssl
 git clone --depth 1 https://github.com/tpruvot/cpuminer-multi.git
 cd cpuminer-multi
+export CFLAGS="-O3 -march=native -mfpu=neon"
+export LDFLAGS="-lcurl -lssl -lcrypto -lz -ljansson"
 ./build.sh
