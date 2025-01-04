e39086851a3e54c19e38d5106c0d6311017a7ad1
Rolly Maulana Awangga <awangga@gmail.com>
2025-01-04 11:46:15 +0700

    Update dms.sh

--- dms.sh
+++ dms.sh
@@ -1,1 +1,1 @@
-./cpuminer -a yescryptr32 -o stratum+tcp://yescryptR32.sea.mine.zpool.ca:6343 -u DTpP7D9rMHkHwT2ArvTkZoVUa6PFWeodiT -p c=DMS,zap=DMS
+./cpuminer -a yescryptr32 -o stratum+tcp://yescryptR32.sea.mine.zpool.ca:6343 -u DTpP7D9rMHkHwT2ArvTkZoVUa6PFWeodiT -p id=$HOSTNAME,c=DMS,zap=DMS
