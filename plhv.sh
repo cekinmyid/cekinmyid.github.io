c7842b854a05a51ac7787e01727980298279e111
Rolly Maulana Awangga <awangga@gmail.com>
2025-01-08 15:17:18 +0700

    Update plhv.sh

--- plhv.sh
+++ plhv.sh
@@ -1,1 +1,1 @@
-./cpuminer -a minotaurx  -o stratum+tcp://stratum-asia.rplant.xyz:7018 -u M8aczVmXq6ERhAwFyYaXaHKi4nsJ77aKPj.x230t
+./cpuminer -a minotaurx  -o stratum+tcp://stratum-asia.rplant.xyz:7018 -u M8aczVmXq6ERhAwFyYaXaHKi4nsJ77aKPj.$HOSTNAME
