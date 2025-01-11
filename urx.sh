1929095c48332a2a7ebb471de04f4a5aecd297a8
Rolly Maulana Awangga <awangga@gmail.com>
2025-01-11 22:52:46 +0700

    Create urx.sh

---
+++ urx.sh
@@ -0,0 +1,1 @@
+./cpuminer --algo yespower --param-n 2048 --param-r 32 --param-key "UraniumX" -o stratum+tcp://yespowerURX.sea.mine.zpool.ca:6236 -u UZn6b46eSorFDpDxpA35KBfZmhV8fSLkcT -p c=URX,zap=URX
