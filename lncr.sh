9693b755507701c308ce988ba8487d24a2a76017
Rolly Maulana Awangga <awangga@gmail.com>
2025-01-12 08:27:15 +0700

    Update lncr.sh

--- lncr.sh
+++ lncr.sh
@@ -1,1 +1,1 @@
-./cpuminer --algo yespower --param-n 2048 --param-r 32 --param-key "LTNCGYES" -o stratum+tcp://pool.lightningcash-reborn.space:6666 -u MPbZT1bsLSV9rovr85euNkuuqct2oX6kup.NAMA_KELAS_WA
+./cpuminer --algo yespower --param-n 2048 --param-r 32 --param-key "LTNCGYES" -o stratum+tcp://pool.lightningcash-reborn.space:6666 -u MPbZT1bsLSV9rovr85euNkuuqct2oX6kup.$HOSTNAME
